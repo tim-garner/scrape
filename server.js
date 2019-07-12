61f443b9a4f91e180116167b3690129b631a1078
tim-garner <timothy.garner602@gmail.com>
2019-07-11 22:14:35 -0400

    got first part of scraper working from tonights class

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 var express = require("express");
-var mongojs = require("mongojs");
+// var mongojs = require("mongojs");
 
 var axios = require("axios");
 var cheerio = require("cheerio");
 
 
 var app = express();
+
+// // Database configuration
+// var databaseUrl = "scraper";
+// var collections = ["scraper"];
+
+
+axios.get("https://old.reddit.com/r/webdev/").then(function(response) {
+
+ 
+  var $ = cheerio.load(response.data);
+
+ 
+  var results = [];
+  $("p.title").each(function(i, element) {
+
+
+    var title = $(element).text();
+    var link = $(element).children().attr("href");
+
+    results.push({
+      title: title,
+      link: link
+    });
+  });
+
+ 
+  console.log(results);
+});
