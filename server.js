c2490a243590321406ebadd10c82cffe3dbe58f2
tim-garner <timothy.garner602@gmail.com>
2019-07-14 16:48:22 -0400

    issues with mongo

--- server.js
+++ server.js
@@ -2,19 +2,17 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
+
 var axios = require("axios");
 var cheerio = require("cheerio");
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-mongoose.connect(MONGODB_URI);
 
 
 var db = require("./models");
 
 var PORT = 3000;
 
-var app = express()
-;
+var app = express();
+
 
 app.use(logger("dev"));
 
@@ -23,103 +21,87 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-console.log("\n******************************************\n" +
-            "Grabbing every article headline and link\n" +
-            "from the website:" +
-            "\n******************************************\n");
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
 
-            axios.get("/scrape", function(req, res){
 
-            
+app.get("/scrape", function(req, res) {
+  axios.get("http://www.echojs.com/").then(function(response) {
+    var $ = cheerio.load(response.data);
 
-            axios.get("https://www.nhl.com/").then(function(response) {
+    $("article h2").each(function(i, element) {
+      var result = {};
 
-              var $ = cheerio.load(response.data);
-            
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
 
-              
+        result.headline = $(this)
+        .children("a")
+        .attr("href");
 
-              $("article h2").each(function(i, element) {
+        result.summary = $(this)
+        .children("a")
+        .attr("href");
 
-                var result = {};
-            
-                result.titletitle = $(this)
-                .children("a")
-                .text("href");
-
-                result.linklink = $(this)
-                .children("a")
-                .attr("href");
-            
-     
-                results.push({
-                  title: title,
-                  link: link,
-                
-      
-                });
-              });
-
-              console.log(results);
-            });
-
-            db.Article.create(result)
-            .then(function(dbArticle) {
-      
-              console.log(dbArticle);
-            })
-            .catch(function(err) {
-   
-              console.log(err);
-            });
-            res.send("Scrape Complete");
-        });
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
     
+          console.log(err);
+        });
+    })
 
-       
- 
+    res.send("Scrape Complete");
+  });
+});
 
 
 app.get("/articles", function(req, res) {
-
   db.Article.find({})
     .then(function(dbArticle) {
       res.json(dbArticle);
     })
     .catch(function(err) {
-      
       res.json(err);
     });
 });
 
 
 app.get("/articles/:id", function(req, res) {
-
+  
   db.Article.findOne({ _id: req.params.id })
+    
     .populate("note")
     .then(function(dbArticle) {
-  
+    
       res.json(dbArticle);
     })
     .catch(function(err) {
-
+  
       res.json(err);
     });
 });
 
 
 app.post("/articles/:id", function(req, res) {
+
   db.Note.create(req.body)
     .then(function(dbNote) {
-  
+
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
     .then(function(dbArticle) {
-  
+
       res.json(dbArticle);
     })
     .catch(function(err) {
-
       res.json(err);
     });
 });
