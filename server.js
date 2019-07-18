407f53a4f0fd2c4e377fb0292efee125d6d2bd36
tim-garner <timothy.garner602@gmail.com>
2019-07-18 00:17:52 -0400

    connected to mongo db, scrape working

--- server.js
+++ server.js
@@ -22,8 +22,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
-mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
 
 
 app.get("/scrape", function(req, res) {
